811c991d98839fb528366407233803e6195a193c
Seni-J <senistan.jegarajasingam@cpnv.ch>
2018-05-09 14:12:30 +0200

    Création du projet

---
+++ Code/desktop/src/com/studentinvaders/tpi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.studentinvaders.tpi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.studentinvaders.tpi.StudentInvaders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StudentInvaders(), config);
+	}
+}
